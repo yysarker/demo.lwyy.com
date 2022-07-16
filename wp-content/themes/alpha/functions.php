51fa8534c028002dd75742025da6a1fa532d01f9
= <=>
2022-07-16 23:26:46 +0600

    Dynamic Page also title, image, tag, date, pagination etc.

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -1,5 +1,21 @@
 <?php
+    /**
+     *  Created by PhpStorm.
+     *  User: yys
+     *  Date: 7/16/2022
+     *  Time: 12:58 PM
+     */
 
-function CaseChange($text){
-    return  strtoupper($text);
-}
+    function alpha_bootstrapping()
+    {
+        load_theme_textdomain('alpha');
+        add_theme_support('post-thumbnails');
+        add_theme_support('title-tag');
+    };
+    add_action("after_setup_theme", "alpha_bootstrapping");
+
+    function alpha_assets(){
+        wp_enqueue_style("alpha", get_stylesheet_uri());
+        wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+    }
+    add_action("wp_enqueue_scripts", "alpha_assets");
--- wp-content/themes/alpha/index.php
+++ wp-content/themes/alpha/index.php
@@ -1,38 +1,89 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title><?php bloginfo('name'); ?></title>
+    <meta charset="UTF-8">
+    <title>Hello Dolly</title>
+    <?php wp_head(); ?>
 </head>
-<body>
-    <div class="content">
-        <h2>All Posts</h2>
-        <hr/>
-        <div class="allPost">
-            <!--This is wordPress loop-->
-            <?php
-                if (have_posts()) {
-                    while (have_posts()){
-                        the_post();
-                        echo "<h3>";
-                            the_title();
-                        echo "</h3>";
-                    }
-                }
-            ?>
-            <!--or we can write as like this-->
-            <hr/>
-
-            <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-                <h3><?php the_title(); ?></h3>
-            <?php endwhile; else:?>
-                <p>Your Post Not found.</p>
-            <?php endif; ?>
-
+<body <?php body_class(); ?>>
+<div class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="tagline"><?php bloginfo("description"); ?></h3>
+                <h1 class="align-self-center display-1 text-center heading"><?php bloginfo("name"); ?></h1>
+            </div>
         </div>
     </div>
+</div>
+<div class="posts">
+    <?php while (have_posts()){
+        the_post();
+    ?>
+    <div class="post" <?php post_class(); ?>>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="post-title"><?php the_title(); ?></h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <p>
+                        <strong><?php the_author(); ?></strong><br/>
+                        <?php the_date(); ?>
+                    </p>
+                    <?php echo get_the_tag_list("<ul class=\"list-unstyled\">
+                        <li>", "</li><li>", "</li></ul>"); ?>
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        <?php
+                            if (has_post_thumbnail()){
+                                the_post_thumbnail('large', array('class' => 'img-fiuid'));
+                            }
+                        ?>
+                    </p>
+                    <p>
+                        <?php the_excerpt(); ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-8">
+                    <?php paginate_links(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+   <?php } ?>
+    <div class="container posts_pagination">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-8">
+                <?php
+                    the_posts_pagination(array(
+                        "screen_reader_text" => " ",
+                        "prev_text" => "New Posts",
+                        "next_text" => "Old Posts"
+                    ));
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy; LWHH - All Rights Reserved
+            </div>
+        </div>
+    </div>
+</div>
+<?php wp_footer(); ?>
 </body>
 </html>
